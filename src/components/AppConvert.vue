9ff256d4d10c3c1c62806021b9e984dcc3a278d8
MarcoSac1 <marcosaccone8@gmail.com>
2024-11-07 16:54:12 +0100

    fixed css issue

--- src/components/AppConvert.vue
+++ src/components/AppConvert.vue
@@ -6,7 +6,7 @@ export default {
             value2: '',
             fromCurrency: 'EUR',
             toCurrency: 'USD',
-            currencies: [],
+            currencies: {}, // Salva valute come oggetto { codice: nomeCompleto }
         };
     },
 
@@ -21,14 +21,13 @@ export default {
             try {
                 const res = await fetch('https://api.frankfurter.app/currencies');
                 const data = await res.json();
-                this.currencies = Object.keys(data); 
+                this.currencies = data; // Salva le valute con nomi completi
             } catch (error) {
                 console.error('Errore nel recupero delle valute:', error);
             }
         },
 
         async updateConversion() {
-            // Pulisce il valore di value2 se value1 è vuoto
             if (!this.value1) {
                 this.value2 = '';
                 return;
@@ -47,47 +46,60 @@ export default {
     },
 
     mounted() {
-        this.fetchCurrencies(); 
-        this.updateConversion(); 
+        this.fetchCurrencies();
+        this.updateConversion();
     },
 };
 </script>
 
-
 <template>
-    <div class="row">
-        <section class="container col-6">
-            <div class="input-group m-4">
+    <div class="row  mt-4 ">
+        <section class="container shadow-lg  justify-content-center align-items-center rounded-2 p-4 col-6">
+            <div class="input-group mb-4">
                 <input type="number" v-model="value1" @input="updateConversion" class="form-control col-6" aria-label="Text input with dropdown button">
-                <button class="btn btn-danger dropdown-toggle col-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">{{ fromCurrency }}</button>
+                <button class="btn btn-danger dropdown-toggle col-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ currencies[fromCurrency] || fromCurrency }}
+                </button>
                 <ul class="dropdown-menu dropdown-menu-end">
-                    <li v-for="(currency, index) in currencies" :key="index">
-                        <a class="dropdown-item" @click="fromCurrency = currency">{{ currency }}</a>
+                    <li v-for="(currencyName, currencyCode) in currencies" :key="currencyCode">
+                        <a class="dropdown-item" @click="fromCurrency = currencyCode">{{ currencyName }}</a>
                     </li>
                 </ul>
             </div>
-            <div class="input-group m-4">
-                <input type="number" v-model="value2" class="form-control col-6" aria-label="Text input with dropdown button">
-                <button class="btn btn-danger dropdown-toggle col-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">{{ toCurrency }}</button>
+            <div class="input-group mb-4">
+                <input type="number" v-model="value2" class="form-control col-
+                6" aria-label="Text input with dropdown button" >
+                <button class="btn btn-danger dropdown-toggle col-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ currencies[toCurrency] || toCurrency }}
+                </button>
                 <ul class="dropdown-menu dropdown-menu-end">
-                    <li v-for="(currency, index) in currencies" :key="index">
-                        <a class="dropdown-item" @click="toCurrency = currency">{{ currency }}</a>
+                    <li v-for="(currencyName, currencyCode) in currencies" :key="currencyCode">
+                        <a class="dropdown-item" @click="toCurrency = currencyCode">{{ currencyName }}</a>
                     </li>
                 </ul>
             </div>
             <div class="m-4">
                 <p id="first-value">
-                    {{ value1 }} {{ fromCurrency }} corrisponde a: 
+                    {{ value1 }} {{ currencies && fromCurrency }} equivale a: 
                 </p>
-                <h1 class="second-value">
-                    {{ value2 }} {{ toCurrency }}
-                </h1>
+                <h2 class="second-value">
+                    {{ value2 }} {{ currencies && toCurrency }}
+                </h2>
             </div>
         </section>
     </div>
 </template>
 
+
 <style scoped>
+    .container{ 
+        font-size: 1rem;
+        background-color: beige;
+    }
+
+    button{
+        font-size: .8;
+    }
     .dropdown-menu {
         max-height: 500px;
         overflow-y: auto;
